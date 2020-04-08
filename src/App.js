eabf32cf32724635cf10d1ecc5c6104666e8292b
noahgiesel <noahgiesel@gmail.com>
2020-04-08 18:50:43 +0200

    fixed all navigation issues

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-       <h1>navigation</h1>
+      <h1>navigation</h1>
+      <h1>navigation</h1>
     </div>
   );
 }
